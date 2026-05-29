3f4969456ca408d40b0912b77ed2c6542c9bb22a
radchukv <radchuk.victoria@gmail.com>
2026-05-29 08:35:55 +0200

    first commit

---
+++ custom_aesthetics.R
@@ -0,0 +1,10 @@
+options(ggplot2.continuous.colour = "viridis")
+options(ggplot2.discrete.colour = "viridis")
+options(ggplot2.fill.colour = "viridis")
+options(ggplot2.fill.colour = "viridis")
+
+colors <- viridis(n = 5, option = "inferno")
+
+gp_colours_full <- c("#313695", "#DD3F4A", "#FFCF37", "#f46d43",  "#74add1")
+gp_colours <- c("#313695", "#DD3F4A", "#FFCF37")
+treat_colours <- c("#B256B4", "#8E2E79")
